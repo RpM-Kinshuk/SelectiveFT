633aff78e4e7bfd0a664a7188b2f0d08d0324a8a
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2024-04-13 13:47:19 +0530

    Fixed bug

--- model.py
+++ model.py
@@ -108,7 +108,7 @@ def get_model(args):
         token="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",
         cache_dir=args.cache_dir,
         padding_side="right",
-        use_fast=False, # Fast tokenizer giving issues.
+        # use_fast=False, # Fast tokenizer giving issues.
         tokenizer_type='llama' if 'llama' in args.model_name_or_path else None, # Needed for HF name change
         trust_remote_code=args.trust_remote_code,
         # use_auth_token=args.use_auth_token,
