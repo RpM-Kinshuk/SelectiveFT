3db9214492cb9b268521e360f6198cd318695f26
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2024-08-07 19:44:46 -0400

    Updated code

--- shells/kinexp.py
+++ shells/kinexp.py
@@ -1,7 +1,5 @@
 import os
 import itertools
-from numpy import sort
-from sklearn.neighbors import sort_graph_by_row_values
 from gputracker.gputracker import get_logger, DispatchThread
 os.environ['MKL_THREADING_LAYER'] = 'gnu'
 
@@ -15,17 +13,18 @@ num_layers = 14
 seed = 42
 seed_list = [42, 43, 44, 45]
 
-sortby_list = ['alpha_mid', 'layer', 'random', 'full']
-sortby_list = ['alpha_mid', 'layer']
+sortby_list = ['alpha_mid', 'layer', 'random']
 
-dataset = 'alpaca'
+dataset = 'glue'
 dataset_list = ['alpaca', 'oasst1', 'self-instruct']
 
-order = 'True'
+task_name = 'cola'
+task_list = ['mrpc', 'cola', 'rte']
+
+order = 'False'
 order_list = ['True', 'False']
 
-grid = itertools.product(dataset_list, sortby_list, order_list)
-
+grid = itertools.product(task_list, sortby_list, order_list)
 # model = 'meta-llama/Meta-Llama-3-8B'
 model = 'meta-llama/Llama-2-7b-hf'
 cachedir = "/scratch/kinshuk/cache"
@@ -34,9 +33,9 @@ logger = get_logger('log', 'schedule_subspace.log')
 # Bash command list
 BASH_COMMAND_LIST = []
 
-for dataset, sortby, order in grid:
+for task_name, sortby, order in grid:
     
-    save_path = "/jumbo/yaoqingyang/kinshuk/LlaMAft/results"
+    save_path = "/jumbo/yaoqingyang/kinshuk/LlaMAft/output"
 
     cmd = (
         "OMP_NUM_THREADS=1 python /jumbo/yaoqingyang/kinshuk/LlaMAft/llamaft.py"
@@ -45,13 +44,14 @@ for dataset, sortby, order in grid:
         f" --cache_dir {cachedir}"
         f" --output_dir {save_path}"
         f" --dataset {dataset}"
+        f" --task_name {task_name}"
         f" --sortby {sortby}"
         f" --sort_ascending {order}"
         f" --num_layers {num_layers}"
         f" --max_steps {steps}"
         f" --data_seed 42"
         f" --source_max_len 1024"
-        f" --learning_rate 2e-6"
+        f" --learning_rate {lr}"
         f" --dataloader_num_workers 1"
         f" --evaluation_strategy steps"
     )
@@ -60,12 +60,13 @@ for dataset, sortby, order in grid:
 
 # Dispatch thread setup
 dispatch_thread = DispatchThread(
-    "Alpaca dataset training",
+    "Llama training",
     BASH_COMMAND_LIST,
     logger,
     gpu_m_th=500,
     gpu_list=gpus,
     maxcheck=0,
+    num_gpus_needed=4
 )
 
 # Start and join the dispatch thread
